ac0ebf779aa03b443785da1855d64d7a1806be6e
Bahadir Yigit <bahadiryigit0307@gmail.com>
2023-06-14 22:22:39 +0300

    BU NASIL KOD

--- XOX/XOX.cpp
+++ XOX/XOX.cpp
@@ -1,15 +1,13 @@
 ﻿#include <iostream>
 
-using namespace std;
-
 //The Player Plays First:
-int player = 1;
+int player = 0;
 
 //If this variable is true ,game continues
 bool isGameOn = true;
 
 //XOX Inputs Will Come here:
-string playground[9] = {"1","2","3","4","5","6","7","8","9"};
+std::string playground[9] = {"1","2","3","4","5","6","7","8","9"};
 
 //Simple Show Playground Function:
 void ShowGround()
@@ -17,12 +15,10 @@ void ShowGround()
     for (int i = 0; i < 9; i++)
     {
         if (i % 3 == 0 && i != 0)
-        {
-            cout << "|" <<endl;
-        }
-        cout << "|" << playground[i];
+            std::cout << "|" <<std::endl;
+        std::cout << "|" << playground[i];
     }
-    cout <<"|" << endl;
+    std::cout <<"|" << std::endl;
 }
 
 //Is Game Ended Function
@@ -32,42 +28,25 @@ int GameControl()
     for (int i = 0; i < 9; i += 3)
     {
         if(playground[i] == playground[i+1] && playground[i] == playground[i + 2])
-        {
-            
             return playground[i] == "x" ? 1 : 2;
-        }
     }
-
     for (int i = 0; i < 3; i ++)
     {
         if (playground[i] == playground[i + 3] && playground[i] == playground[i + 6])
-        {
             return playground[i] == "x" ? 1 : 2;
-        }
     }
-    
     if ((playground[0] == playground[4] && playground[0] == playground[8]) || (playground[2] == playground[4] && playground[2] == playground[6])) 
-    {
         return (playground[0]  == "x") || (playground[2] == "x") ? 1 : 2;
-    }
     for (int i = 0; i < 9; i++)
     {
         
         if (playground[i] == "x"|| playground[i] == "o")
-        {
             xoFinder += 1;
-            
-        }
     }
     if (xoFinder == 9)
-    {
         return 0;
-    }
     else
-    {
         return 3;
-    }
-    
 }
 
 int main()
@@ -78,44 +57,28 @@ int main()
     while (isGameOn) 
     {
         ShowGround();
-
-        if (player == 1)
-        {
-            do {
-             cout << "Player 1's turn (please write the number which you want) ";
-             cin >> indexInput;
-            } while (playground[indexInput-1] == "x" || playground[indexInput-1] == "o");
-             
-             playground[indexInput-1] = "x";
-             player = 2;
-        }
-        else if (player == 2)
-        {
-            do {
-                cout << "Player 2's turn (please write the number which you want) ";
-                cin >> indexInput;
-            } while (playground[indexInput-1] == "x" || playground[indexInput-1] == "o");
+        do {
+            std::cout << "Player "<< player + 1 << "'s turn (please write the number which you want) ";
+            std::cin >> indexInput;
+        } while (playground[indexInput-1] == "x" || playground[indexInput-1] == "o");
+        if (!player)
+            playground[indexInput-1] = "x";
+        else
             playground[indexInput-1] = "o";
-            player = 1;
-        }
-        if (GameControl() == 1)
+        player++;
+        player %= 2;
+        if (!GameControl())
         {
             ShowGround();
-            cout << "Player 1 Won!";
+            std::cout << "Draw!";
             break;
         }
-        else if(GameControl() == 2)
+        else if (GameControl() == 3);
+        else
         {
             ShowGround();
-            cout << "Player 2 Won!";
-            break;
-        }
-        else if (GameControl() == 0)
-        {
-            ShowGround();
-            cout << "Draw!";
+            std::cout << "Player " << GameControl() << " Won!";
             break;
         }
     }
 }
-
